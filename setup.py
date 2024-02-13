50e689b051a01634f321367ace334f7e6dd1197f
Robert Cudmore <robert.cudmore@gmail.com>
2024-02-13 13:23:01 -0800

    bumped to version 1.0.27 to fix publish to PyPi

--- setup.py
+++ setup.py
@@ -7,7 +7,7 @@ from transonic.dist import ParallelBuildExt, make_backend_files, init_transonic_
 here = Path(__file__).parent.absolute()
 sys.path.insert(0, ".")
 
-VERSION = "1.0.16"
+VERSION = "1.0.27"
 
 TRANSONIC_BACKEND = "pythran"
 
